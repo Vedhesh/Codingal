b3a6899c93822d51da8794a6b109540247b8719e
Vedhesh <75579484+Vedhesh@users.noreply.github.com>
2025-02-17 17:46:28 +0530

    Create project.py
    project

---
+++ OOPS/project.py
@@ -0,0 +1,10 @@
+decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+prompt = int(input('>'))
+returned = ''
+for i in range(len(decimal)):
+    m=prompt//decimal[i]
+    for j in range(m):
+        returned += roman[i]
+    n=n%decimal[i]
+print(f">{returned}")
