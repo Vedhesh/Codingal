30303edf782b1df1576842c0ef0e2625dd59314a
Vedhesh <75579484+Vedhesh@users.noreply.github.com>
2025-02-20 13:45:58 +0530

    fix of dimensions
    asfasf

--- PYGAME/CLASS-1-PYGAME-WINDOW/PROJECT/main.py
+++ PYGAME/CLASS-1-PYGAME-WINDOW/PROJECT/main.py
@@ -10,19 +10,19 @@ display_surface.fill(bg_color)
 
 image = pygame.image.load("PYGAME\CLASS-1-PYGAME-WINDOW\PROJECT\\fixed_alpha.png")
 image_scale = pygame.transform.scale(image.convert_alpha(),(SCREEN_WIDTH//2,SCREEN_HEIGHT//2))
-image_rect = image_scale.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT//2 - 30))
+image_rect = image_scale.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT//2))
 
 text_my = pygame.font.Font(None, 40).render("My", True, pygame.Color("white"))
-text_my_rect = text_my.get_rect(center=(SCREEN_WIDTH//2 - 60, SCREEN_HEIGHT//2 + 120))
+text_my_rect = text_my.get_rect(center=(SCREEN_WIDTH//2 - 60, SCREEN_HEIGHT//2 + 140))
 
 text_first = pygame.font.Font(None, 40).render("first", True, pygame.Color("white"))
 text_first_rect = text_first.get_rect(center=(SCREEN_WIDTH//2 - 20, SCREEN_HEIGHT//2 + 160))
 
 text_game = pygame.font.Font(None, 40).render("game", True, pygame.Color("white"))
-text_game_rect = text_game.get_rect(center=(SCREEN_WIDTH//2 + 20, SCREEN_HEIGHT//2 + 200))
+text_game_rect = text_game.get_rect(center=(SCREEN_WIDTH//2 + 20, SCREEN_HEIGHT//2 + 180))
 
 text_screen = pygame.font.Font(None, 40).render("screen.", True, pygame.Color("white"))
-text_screen_rect = text_screen.get_rect(center=(SCREEN_WIDTH//2 + 60, SCREEN_HEIGHT//2 + 240))
+text_screen_rect = text_screen.get_rect(center=(SCREEN_WIDTH//2 + 60, SCREEN_HEIGHT//2 + 200))
 
 
 def render_loop():
@@ -39,6 +39,7 @@ def render_loop():
         display_surface.blit(text_my,text_my_rect)
         display_surface.blit(text_first,text_first_rect)
         display_surface.blit(text_game,text_game_rect)
+        display_surface.blit(text_screen,text_screen_rect)
         pygame.display.flip()
         clock.tick(30)
 
