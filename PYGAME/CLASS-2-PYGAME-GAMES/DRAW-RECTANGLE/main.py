4da5169ea733b75e3fd276aad7b76eda47c01ca5
Vedhesh <75579484+Vedhesh@users.noreply.github.com>
2025-02-20 18:24:16 +0530

    fix
    3112

--- PYGAME/CLASS-2-PYGAME-GAMES/DRAW-RECTANGLE/main.py
+++ PYGAME/CLASS-2-PYGAME-GAMES/DRAW-RECTANGLE/main.py
@@ -7,6 +7,5 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-            pygame.quit()
     rectangle = pygame.draw.rect(surface, pygame.Color("blue"), pygame.Rect(30,30,60,60))
     pygame.display.flip()
