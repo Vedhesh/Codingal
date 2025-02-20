2eb48b3398fea353dd1364b42d9761af9863799b
Vedhesh <75579484+Vedhesh@users.noreply.github.com>
2025-02-20 18:20:23 +0530

    ac1
    sadasf

---
+++ PYGAME/CLASS-2-PYGAME-GAMES/DRAW-RECTANGLE/main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+surface = pygame.display.set_mode((400,500))
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            done = True
+    rectangle = pygame.draw.rect(surface, pygame.Color("blue"), pygame.Rect(30,30,60,60))
+    pygame.display.flip()
