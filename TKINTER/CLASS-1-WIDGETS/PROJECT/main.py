ef45f2f840e450c334e82fd0f0d92e194b038119
Vedhesh <75579484+Vedhesh@users.noreply.github.com>
2025-04-22 16:26:29 +0530

    vsdafg

---
+++ TKINTER/CLASS-1-WIDGETS/PROJECT/main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+window = Tk()
+text_area = Text(height=20)
+def display():
+    v1 = entry.get()
+    v2 = entry2.get()
+    text_area.insert(END,f"{int(v1)*int(v2)}\n")
+
+welcome = Label(text="Multiplier", fg="white",bg="blue", width=100)
+guide = Label(text="Enter Value 1", fg="gray", width=100)
+entry = Entry(width=60)
+guide2 = Label(text="Enter Value 2", fg="gray", width=100)
+entry2 = Entry(width=60)
+button = Button(text="Formulate the message", command=display, bg="skyblue")
+welcome.pack()
+guide.pack()
+entry.pack()
+guide2.pack()
+entry2.pack()
+text_area.pack()
+button.pack(pady=10)
+window.mainloop()
