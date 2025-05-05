e563807b4183e97a38de4eb4aae621081737a9b9
Vedhesh <75579484+Vedhesh@users.noreply.github.com>
2025-05-05 18:29:14 +0530

    basic

---
+++ TKINTER/CLASS-5-TOPLEVEL/DENOMINATION/main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+root = Tk()
+root.geometry("500x500")
+root.title("Denomination New")
+root.configure(bg="light blue")
+
