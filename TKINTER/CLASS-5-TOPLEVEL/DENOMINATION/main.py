ef69b3c8cb974b354093d4ec63c91ee35644f65d
Vedhesh <75579484+Vedhesh@users.noreply.github.com>
2025-05-05 18:46:54 +0530

    done

--- TKINTER/CLASS-5-TOPLEVEL/DENOMINATION/main.py
+++ TKINTER/CLASS-5-TOPLEVEL/DENOMINATION/main.py
@@ -3,7 +3,24 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 
 root = Tk()
-root.geometry("500x500")
+root.geometry("650x400")
 root.title("Denomination New")
 root.configure(bg="light blue")
 
+upload = Image.open("TKINTER\CLASS-5-TOPLEVEL\DENOMINATION\\fixed_alpha.png")
+upload.thumbnail((300, 300))
+
+image = ImageTk.PhotoImage(upload)
+
+label = Label(master=root, image=image, bg="light blue")
+label1 = Label(master=root, text="Hey User, Welcome to Denomination Calculator [UPDATED].", bg="light blue")
+
+label.place(x=180, y=20)
+label1.place(relx=0.5, y=340, anchor=CENTER)
+
+
+
+
+
+
+root.mainloop()
