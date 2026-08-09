ec8fe952a3514fb5fc0f386e9f1acda79108f956
Vedhesh <75579484+Vedhesh@users.noreply.github.com>
2026-08-09 12:29:47 +0530

    Add files via upload

---
+++ Robots.py
@@ -0,0 +1,11 @@
+class Robot:
+    def __init__(self, name):
+        self.name = name
+    def introduce(self):
+        print(f"My name is {self.name}!")
+
+tom = Robot("Tom")
+jerry = Robot("Jerry")
+
+tom.introduce()
+jerry.introduce()
